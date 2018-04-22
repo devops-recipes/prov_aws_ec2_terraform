7d077283559f2d2dcd23bccf1ebcb6adeb0e33cf
Avi Cavale <avi@shippable.com>
2018-04-22 10:57:40 -0700

    initial commit

---
+++ ec2.tf
@@ -0,0 +1,30 @@
+# Setup our aws provider
+provider "aws" {
+  access_key  = "${var.aws_access_key_id}"
+  secret_key  = "${var.aws_secret_access_key}"
+  region      = "${var.vpc_region}"
+}
+
+# instances
+resource "aws_instance" "ec2Instances" {
+
+  count = "${var.inst_count}"
+  ami = "${var.inst_ami}"
+  availability_zone = "${lookup(var.availability_zone, var.vpc_region)}"
+  instance_type = "${var.inst_type}"
+  key_name = "${var.aws_key_name}"
+  subnet_id = "${var.vpc_public_sn_id}"
+  associate_public_ip_address = true
+  source_dest_check = false
+
+  security_groups = [
+    "${var.vpc_public_sg_id}"]
+
+  tags = {
+    Name = "ec2Instances${count.index}"
+  }
+}
+
+output "ec2_ins_0_ip" {
+  value = "${aws_instance.ec2Instances.0.public_ip}"
+}
